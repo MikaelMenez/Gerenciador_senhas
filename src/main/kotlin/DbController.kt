f056a0ba950997f755c86488a7618ebcb3971f6c
MikaelMenez <mixaelmenezes@gmail.com>
2025-09-04 10:04:55 -0300

    funções do banco de dados

--- src/main/kotlin/DbController.kt
+++ src/main/kotlin/DbController.kt
@@ -1,48 +1,80 @@
 import java.sql.DriverManager
+
 class DbController {
     companion object {
-
-        val connection= DriverManager.getConnection("jdbc:sqlite:gerenciador.db")
-        val statement= connection.createStatement()
+        val key="1234567890123456"
+        val iv="abcdefghijklmnop"
+        val connection = DriverManager.getConnection("jdbc:sqlite:gerenciador.db")
+        val statement = connection.createStatement()
 
         fun createDB(): java.lang.Exception? {
             try {
-                Class.forName("org.sqlite.JDBC")
-                this.statement.execute("""
+                this.statement.execute(
+                    """
                     CREATE TABLE IF NOT EXISTS aplicativos(
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     nome TEXT NOT NULL,
                     senha TEXT UNIQUE NOT NULL
                     )
-                   """.trimIndent())
+                   """.trimIndent()
+                )
                 return null
-            }
-            catch (e: Exception){
+            } catch (e: Exception) {
                 return e
             }
         }
 
-        fun insertData(nome: String,senha: String){
-            Class.forName("org.sqlite.JDBC")
-            val insertUsuarios=connection.prepareStatement(
-                "INSERT INTO aplicativos (nome,senha) VALUES (?, ?)")
-            insertUsuarios.setString(1,nome)
-            insertUsuarios.setString(2,senha)
+        fun insertData(nome: String, senha: String) {
+            val insertUsuarios = connection.prepareStatement(
+                "INSERT INTO aplicativos (nome,senha) VALUES (?, ?)"
+            )
+            insertUsuarios.setString(1, nome)
+            insertUsuarios.setString(2, CriptografyController.encrypt(senha,iv,key))
             insertUsuarios.executeUpdate()
         }
-        fun showAll(){
-            Class.forName("org.sqlite.JDBC")
+
+        fun showAll() {
             val resultSet = statement.executeQuery("SELECT * FROM aplicativos")
             while (resultSet.next()) {
-                println("ID: ${resultSet.getInt("id")}, Nome: ${resultSet.getString("nome")}, Senha: ${resultSet.getString("senha")}")
+                println(
+                    "ID: ${resultSet.getInt("id")}, Nome: ${resultSet.getString("nome")}, Senha: ${
+                        resultSet.getString(
+                            "senha"
+                        )
+                    }"
+                )
             }
         }
-        fun getOne(id: Int):Senhas{
-            Class.forName("org.sqlite.JDBC")
-            val resultSet = statement.executeQuery("SELECT * FROM aplicativos WHERE id="+id)
-            val senha=Senhas(resultSet.getString("senha"),resultSet.getString("nome"))
+
+        fun getOne(id: Int): Senhas {
+            val resultSet = statement.executeQuery("SELECT * FROM aplicativos WHERE id=" + id)
+            val senha = Senhas(resultSet.getString("senha"), resultSet.getString("nome"))
             return senha
         }
 
-    }
+        fun modifyName(nome: String, id: Int) {
+            val sql = "UPDATE aplicativos SET nome = ? WHERE id = ?"
+            connection.prepareStatement(sql).use { stmt ->
+                stmt.setString(1, nome)
+                stmt.setInt(2, id)
+                stmt.executeUpdate()
+            }
+        }
+
+        fun modifyPassword(senha: String, id: Int) {
+            val sql = "UPDATE aplicativos SET senha = ? WHERE id = ?"
+            connection.prepareStatement(sql).use { stmt ->
+                stmt.setString(1, senha)
+                stmt.setInt(2, id)
+                stmt.executeUpdate()
+            }
+        }
+        fun deleteById(id: Int) {
+            val sql = "DELETE FROM aplicativos WHERE id = ?"
+            connection.prepareStatement(sql).use { stmt ->
+                stmt.setInt(1, id)
+                stmt.executeUpdate()
+            }
+        }
+}
 }
